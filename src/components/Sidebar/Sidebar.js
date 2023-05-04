071d743711d93d41581b207cfed757a1ed64c9d5
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2023-05-04 13:22:25 +0300

    update

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
 import {
   FaTh,
   FaBars,
@@ -10,122 +12,100 @@ import {
 } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
-class Sidebar extends Component {
-  constructor({ children }) {
-    super();
-    this.state = {
-      isOpen: false,
-      children: children,
-    };
-  }
+const Sidebar = ({ children }) => {
+  const navigate = useNavigate();
 
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  };
-
-  signOut = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const signOut = () => {
     localStorage.removeItem("jwt");
     localStorage.clear();
-    //setTimeout(() => window.location.reload(), 1200);
-    window.location.reload();
+    navigate("/");
+
+    setTimeout(() => window.location.reload(), 1200);
+    //window.location.reload();
   };
 
-  render() {
-    const menuItem = [
-      {
-        path: "/dashboard",
-        name: "Dashboard",
-        icon: <FaTh />,
-      },
-      {
-        path: "/employees",
-        name: "Employees",
-        icon: <FaUsers />,
-      },
-      {
-        path: "/addemployee",
-        name: "Add new employee",
-        icon: <FaUserPlus />,
-      },
-      {
-        path: "/sendmessage",
-        name: "Send Message",
-        icon: <FaCommentAlt />,
-      },
-      {
-        path: "/profile",
-        name: "Profile",
-        icon: <FaUserCircle />,
-      },
-      // {
-      //   path: "/login",
-      //   name: "Sign Out",
-      //   icon: <FaUserCircle />,
-      // },
-    ];
-
-    return (
-      <div className="container">
-        <div
-          style={{ width: this.state.isOpen ? "200px" : "50px" }}
-          className="sidebar"
-        >
-          <div className="top_section">
-            <h1
-              style={{ display: this.state.isOpen ? "block" : "none" }}
-              className="logo"
-            >
-              Logo
-            </h1>
-            <div
-              style={{ marginLeft: this.state.isOpen ? "50px" : "0px" }}
-              className="bars"
-            >
-              <FaBars onClick={this.toggle} />
-            </div>
+  const menuItem = [
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <FaTh />,
+    },
+    {
+      path: "/employees",
+      name: "Employees",
+      icon: <FaUsers />,
+    },
+    {
+      path: "/addemployee",
+      name: "Add new employee",
+      icon: <FaUserPlus />,
+    },
+    {
+      path: "/sendmessage",
+      name: "Send Message",
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      icon: <FaUserCircle />,
+    },
+    // {
+    //   path: "/login",
+    //   name: "Sign Out",
+    //   icon: <FaUserCircle />,
+    // },
+  ];
+  return (
+    <div className="container">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
+            Logo
+          </h1>
+          <div style={{ marginLeft: isOpen ? "50px" : "0px" }} className="bars">
+            <FaBars onClick={toggle} />
           </div>
-          {menuItem.map((item, index) => (
-            <NavLink
-              to={item.path}
-              key={index}
-              className="link"
-              activeclassname="active"
-            >
-              <div className="icon">{item.icon}</div>
-              <div
-                style={{
-                  display: this.state.isOpen ? "block" : "none",
-                }}
-                className="link_text"
-              >
-                {item.name}
-              </div>
-            </NavLink>
-          ))}
+        </div>
+        {menuItem.map((item, index) => (
           <NavLink
-            to=""
+            to={item.path}
+            key={index}
             className="link"
             activeclassname="active"
-            onClick={this.signOut}
-            style={{ marginTop: "auto" }}
           >
-            <div className="icon">
-              <FaSignOutAlt />
-            </div>
+            <div className="icon">{item.icon}</div>
             <div
-              style={{ display: this.state.isOpen ? "block" : "none" }}
+              style={{ display: isOpen ? "block" : "none" }}
               className="link_text"
             >
-              Sign Out
+              {item.name}
             </div>
           </NavLink>
-        </div>
-        <main>{this.state.children}</main>
+        ))}
+        <NavLink
+          to=""
+          className="link"
+          activeclassname="active"
+          onClick={signOut}
+          style={{ marginTop: "auto" }}
+        >
+          <div className="icon">
+            <FaSignOutAlt />
+          </div>
+          <div
+            style={{ display: isOpen ? "block" : "none" }}
+            className="link_text"
+          >
+            Sign Out
+          </div>
+        </NavLink>
       </div>
-    );
-  }
-}
+      <main>{children}</main>
+    </div>
+  );
+};
 
 export default Sidebar;

c10ab6dbccd60a6844b02cf38cee4c5f1789a782
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2024-01-14 20:12:13 +0200

    initial commit

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -1,146 +1,118 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { QRCodeSVG } from "qrcode.react";
 import axios from "axios";
 import Select from "react-select";
 
-class SendMessage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      qrcode: false,
-      message: "",
-      contactsOptions: [],
-      sendTo: [],
+const SendMessage = ({ contacts }) => {
+  //const [loading, setLoading] = useState(false);
+  const [qrcode, setQRCode] = useState(false);
+  const [message, setMessage] = useState("");
+  const [contactsOptions, setContactsOptions] = useState([]);
+  const [sendTo, setSendTo] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3001/qrcode");
+        if (data) {
+          //setLoading(true);
+          setQRCode(data);
+        }
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        //setLoading(false);
+      }
     };
-  }
+    fetchData();
+  }, []);
 
-  sendMessage = async () => {
+  const sendMessage = async () => {
     let phones = [];
-    const message = this.state.message;
-    this.state.sendTo.forEach((contact) => {
-      phones.push(`+4${contact.value}`);
+    sendTo.forEach((contact) => {
+      phones.push(`4${contact.value}@c.us`);
     });
 
-    // const res = await axios.post(
-    //   "http://localhost:3001/send",
-
-    //   {
-    //     phones,
-    //     message,
-    //   },
-    //   {
-    //     headers: {
-    //       "Content-Type": "application/x-www-form-urlencoded",
-    //     },
-    //   }
-    // );
-
-    this.setState({
-      loading: true,
-    });
-
-    const res = await axios.post(
-      "https://casutaursitoarelor-api.onrender.com/send",
-      {
+    //setLoading(true);
+    console.log(phones);
+    try {
+      await axios.post("https://casutaursitoarelor-api.onrender.com/send", {
         phones,
         message,
-      }
-    );
-    this.setState({
-      qrcode: res.data,
-    });
-    this.setState({
-      loading: false,
-    });
-  };
-
-  getMessage = (e) => {
-    this.setState({
-      message: e.target.value,
-    });
-  };
-
-  componentDidMount = () => {
-    const contactsList = [];
-    const contacts = this.props.contacts;
-    contacts.forEach((contact) => {
-      contactsList.push({
-        value: contact.phoneNumber,
-        label: contact.employeeName,
       });
-    });
-    this.setState({
-      contactsOptions: contactsList,
-    });
+      //setQRCode(res.data);
+    } catch (error) {
+      console.error("Error sending message:", error);
+    } finally {
+      //setLoading(false);
+    }
   };
 
-  // getRecepient = (data) => {
-  //   //console.log(data);
-  // };
-
-  getUserChoice = (choice) => {
-    this.setState({
-      sendTo: choice,
-    });
+  const getMessage = (e) => {
+    setMessage(e.target.value);
   };
 
-  render() {
-    const colourStyles = {
-      control: (styles) => ({ ...styles, width: "500px" }),
-    };
-    // const { contacts } = this.state;
+  useEffect(() => {
+    const contactsList = contacts.map((contact) => ({
+      value: contact.phoneNumber,
+      label: contact.employeeName,
+    }));
+    setContactsOptions(contactsList);
+  }, [contacts]);
 
-    return (
-      <div className="mt5">
-        {!this.state.loading && this.state.qrcode && (
-          <div className="tc ma5">
-            <div>
-              <QRCodeSVG value={this.state.qrcode} />
-            </div>
-          </div>
-        )}
-        {/* <SearchContact contacts={contacts} sendMsg={this.getRecepient} /> */}
-        <div className="center">
-          <Select
-            isMulti
-            placeholder="Search Contact :"
-            name="colors"
-            options={this.state.contactsOptions}
-            className="basic-multi-select"
-            classNamePrefix="select"
-            styles={colourStyles}
-            onChange={(choice) => this.getUserChoice(choice)}
-          />
-        </div>
-        <div className="pa4 black-80 center">
+  const colourStyles = {
+    control: (styles) => ({ ...styles, width: "500px" }),
+  };
+
+  return (
+    <div className="mt5">
+      {qrcode && (
+        <div className="tc ma5">
           <div>
-            <label htmlFor="message" className="tc f6 b db mb2">
-              Message
-            </label>
+            <QRCodeSVG value={qrcode} />
+          </div>
+        </div>
+      )}
+      <div className="center">
+        <Select
+          isMulti
+          placeholder="Search Contact :"
+          name="colors"
+          options={contactsOptions}
+          className="basic-multi-select"
+          classNamePrefix="select"
+          styles={colourStyles}
+          onChange={(choice) => setSendTo(choice)}
+        />
+      </div>
+      <div className="pa4 black-80 center">
+        <div>
+          <label htmlFor="message" className="tc f6 b db mb2">
+            Message
+          </label>
 
-            <textarea
-              id="message"
-              name="message"
-              className="db border-box hover-black  measure ba b--black-20 pa2 br2 mb2"
-              aria-describedby="message-desc"
-              rows="13"
-              style={{ width: "500px" }}
-              onChange={this.getMessage}
-            ></textarea>
-            <div className="tc">
-              <input
-                onClick={() => this.sendMessage()}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Send"
-              />
-            </div>
+          <textarea
+            id="message"
+            name="message"
+            className="db border-box hover-black  measure ba b--black-20 pa2 br2 mb2"
+            aria-describedby="message-desc"
+            rows="13"
+            style={{ width: "500px" }}
+            onChange={getMessage}
+          ></textarea>
+          <div className="tc">
+            <input
+              onClick={sendMessage}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Send"
+            />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SendMessage;

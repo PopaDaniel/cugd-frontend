96c563aff50f1beb870eb73ff80ac395ab511127
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2024-06-25 18:46:33 +0300

    last commit

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -23,15 +23,14 @@ const SendMessage = ({ contacts }) => {
   useEffect(() => {
     const checkConnectionStatus = async () => {
       try {
-        const response = await axios.get(
-          "https://casutaursitoarelor-api.onrender.com/status"
-        );
+        const response = await axios.get("http://localhost:3001/status");
         setConnected(response.data.connected);
         setStatus(
           response.data.connected ? "Connected to WhatsApp" : "Not connected"
         );
-        if (response.data.connected) {
+        if (response.data.connected && qrTimeout) {
           clearTimeout(qrTimeout);
+          setQrTimeout(null);
         }
       } catch (error) {
         console.error("Error checking connection status:", error);
@@ -46,9 +45,7 @@ const SendMessage = ({ contacts }) => {
 
   const fetchQRCode = async () => {
     try {
-      const response = await axios.get(
-        "https://casutaursitoarelor-api.onrender.com/connect"
-      );
+      const response = await axios.get("http://localhost:3001/connect");
       if (response.data.qr) {
         setQRCode(response.data.qr);
         setStatus("Scan the QR code");
@@ -66,14 +63,13 @@ const SendMessage = ({ contacts }) => {
 
   const handleConnect = async () => {
     clearTimeout(qrTimeout);
+    setQrTimeout(null);
     fetchQRCode();
   };
 
   const handleDisconnect = async () => {
     try {
-      const response = await axios.get(
-        "https://casutaursitoarelor-api.onrender.com/disconnect"
-      );
+      const response = await axios.get("http://localhost:3001/disconnect");
       setStatus(response.data.message);
       setConnected(false);
       setQRCode(null);
@@ -99,13 +95,10 @@ const SendMessage = ({ contacts }) => {
     let phones = sendTo.map((contact) => `4${contact.value}@c.us`);
 
     try {
-      const res = await axios.post(
-        "https://casutaursitoarelor-api.onrender.com/send",
-        {
-          phones,
-          message,
-        }
-      );
+      const res = await axios.post("http://localhost:3001/send", {
+        phones,
+        message,
+      });
       if (res.data.failed && res.data.failed.length > 0) {
         setNotification({
           show: true,
